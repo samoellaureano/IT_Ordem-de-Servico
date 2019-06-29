4d6be4b84442efb1309d31844b6801aeb30150a3
samoel <samoellaureano@gmail.com>
2019-06-28 22:31:40 -0300

    Fundo escuro do menu

--- Assistencia/WebContent/resources/menu/js/menu.js
+++ Assistencia/WebContent/resources/menu/js/menu.js
@@ -1,17 +1,34 @@
 assistencia = new Object();
 
+$(document).ready(function(){
+    document.getElementById("fundoMenu").style.display = 'none';
+
+    if($(".label").html() == "Cliente"){
+        document.getElementById("novaOS").innerHTML = "";
+        document.getElementById("OSAberto").innerHTML = "";
+        document.getElementById("produtos").innerHTML = "";
+        document.getElementById("servicos").innerHTML = "";
+        document.getElementById("consulta").innerHTML = "";
+        document.getElementById("users").innerHTML = "";
+    }
+});
+
 assistencia.search = function(){
-    var ordemServico = $("#ordemServico").val();
-    if(ordemServico != ""){
+    if($("#ordemServico").val() != ""){
         alert(ordemServico);
-        var Cliente = "Cliente";
-        alteraLabel(Cliente);
+        document.getElementById("ordemServico").value = "";
     }else{
         return false;
     }
     
 }
 
-function alteraLabel(texto) {
-    document.getElementById("perfil").innerHTML =texto;
-  }
+function alteraLabel() {//Altera a label do tipo de perfil que aparene abaixo do nome
+    var cliente = "Cliente";
+    //alteraLabel(Cliente);
+    document.getElementById("perfil").innerHTML = cliente;
+}
+
+function fundoMenu(){
+    document.getElementById("fundoMenu").style.display = 'block';
+}
